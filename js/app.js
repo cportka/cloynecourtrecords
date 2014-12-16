2420b84597fc61ca603528eef6f9b2c02955ba91
Christopher Rhiao <chris@rhiao.com>
2014-12-16 02:10:41 -0800

    bunch of shit added

--- js/app.js
+++ js/app.js
@@ -1,17 +1,39 @@
-headerTxt = $('header').html();
+headerTxt = $('header p');
+imgCCR = $('header img');
 
 $(window).scroll(function () {
-    var imgCCR = $("<img src='images/ccr.png'>");
-
-    if ($(this).scrollTop() > 1) {
-        $('header').html("");
+    if ($(this).scrollTop() > 0) {
         if (!$('header').hasClass("sticky")) {
-            $('header').append(imgCCR);
+            $('header p').remove();
+            $('header').prepend(imgCCR);
+            $('header').addClass("sticky");
+            $('.header-links').css('float', "right");
         }
-        $('header').addClass("sticky");
     } else {
-        $('header').html(headerTxt);
-        $('header').removeClass("sticky");
-        $('header img').remove();
+        if ($('header').hasClass("sticky")) {
+            $('header').prepend(headerTxt);
+            $('header').removeClass("sticky");
+            $('header img').remove();
+            $('.header-links').css('float', "none");
+        }
     }
+});
+
+$(function () {
+    $('a[href*=#]:not([href=#])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
+
+$(document).ready(function () {
+    $('header img').remove();
 });
